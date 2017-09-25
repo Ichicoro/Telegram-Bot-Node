44ca90c2304e13a158b294fd2d54227f98c312b6
Ichicoro <ichi@ichicoro.pw>
2017-09-25 22:18:53 +0200

    Convert ReverseAudio.js and Rule34.js to the new syntax

--- src/plugins/ReverseAudio.js
+++ src/plugins/ReverseAudio.js
@@ -21,11 +21,9 @@ module.exports = class ReverseAudio extends Plugin {
 
     onCommand({message, command, args}, reply) {
         if (command !== "reverseaudio" && command !== "reverse") return;
-        if (!message.hasOwnProperty('reply_to_message')) return reply({
-            type: "text",
-            text: "You need to reply to an audio with this command."
-        });
-        console.log("it's reverseaudio!");
+        if (!message.hasOwnProperty('reply_to_message')) 
+            return this.sendMessage(message.chat.id, "You need to reply to an audio with this command.");
+        this.log.debug("received reverseaudio!");
         let target;
         let isVoice = false;
         if (message.reply_to_message.hasOwnProperty("voice")) {
@@ -34,27 +32,21 @@ module.exports = class ReverseAudio extends Plugin {
         } else if (message.reply_to_message.hasOwnProperty("audio")) {
             target = message.reply_to_message.audio.file_id;
         } else {
-            reply({
-                type: "text",
-                text: "Welp, I want an audio/voice message, anything else won't work :c"
-            })
+            this.sendMessage(message.chat.id, "Welp, I want an audio/voice message, anything else won't work :c");
             return;
         }
 
-        console.log("target: ");
-        console.log(target);
+        this.log.debug("target: ");
+        this.log.debug(target);
 
         this.bot.getFile(target).then((file) => {
             const token = JSON.parse(require("fs").readFileSync("./config.json", "utf8")).TELEGRAM_TOKEN;
-            console.log(this.bot.config)
+            //this.log.debug(this.bot.config)
             const fileurl = `https://api.telegram.org/file/bot${token}/${file.file_path}`;
             Util.downloadAndSaveTempResource(fileurl, "ogg", (filepath) => {
                 const fn = `/tmp/${Util.makeUUID()}.ogg`;
                 child_process.spawnSync(ffmpeg, ["-i", filepath, "-af", "areverse", fn]);
-                reply({
-                    type: "audio",
-                    audio: fn
-                })
+                this.sendAudio(message.chat.id, fn);
             })
                 /* fs.readFile(filepath, 'utf8', (err, data) => {
                     if (err) return console.log(err);
